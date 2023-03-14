1c47baedbbeb38d69afb3098e20f88a9390e40a4
YoungBeomLee <edlee127127@gmail.com>
2023-03-14 18:20:05 +0900

    03-14 이영범 배너제작중 swiper

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import HeaderFT from "./components/Common/HeaderFT";
 import FooterFT from "./components/Common/FooterFT";
+import Swiper  from "./components/Common/BannerFT";
 
 function App() {
   return (
     <div className='App'>
       <HeaderFT></HeaderFT>
+      <Swiper />
       <FooterFT></FooterFT>
+      
     </div>
   );
 }
