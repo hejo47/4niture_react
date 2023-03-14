917c69ee8bc6eb7d00109a939e2a37d9784d863f
hejo47 <hejo_47@naver.com>
2023-03-14 16:49:27 +0900

    230313 [강혜주] login 제작

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import HeaderFT from "./components/Common/HeaderFT";
 import FooterFT from "./components/Common/FooterFT";
 import MainLongTabFT from "./components/pages/MainLongTabFT";
 import ReviewsFT from "./components/pages/ReviewsFT";
+import LoginAD from "./components/pages/LoginAD";
+import { Routes, Route } from "react-router-dom";
+import ContainerFT from "./components/UI/ContainerFT";
 function App() {
   return (
     <div className='App'>
+      <Routes>
+        <Route
+          path='/login'
+          element={
+            <>
+              <ContainerFT>
+                <div style={{ maxWidth: 500, margin: "0 auto", paddingTop: 200, paddingBottom: 200 }}>
+                  <LoginAD></LoginAD>
+                </div>
+              </ContainerFT>
+              <FooterFT></FooterFT>
+            </>
+          }
+        />
+      </Routes>
       <HeaderFT></HeaderFT>
 
       <MainLongTabFT></MainLongTabFT>
