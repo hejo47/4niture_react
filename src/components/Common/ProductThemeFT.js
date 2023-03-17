81b8e5184c4bdad5dbe716527a9925fa8c11f53c
hejo47 <hejo_47@naver.com>
2023-03-17 17:51:29 +0900

    230317 [강혜주] 라우팅 경로 바꾸는 중

--- src/components/Common/ProductThemeFT.js
+++ src/components/Common/ProductThemeFT.js
@@ -1,20 +1,22 @@
 import React from "react";
 import classes from "./ProductThemeFT.module.css";
-import {Link} from "react-router-dom";
-import nh_test from "../../img/nh_test.jpg"
+import { Link } from "react-router-dom";
+import nh_test from "../../img/nh_test.jpg";
 
-const ProductThemeFT=(props)=>{
-    return(
+const ProductThemeFT = (props) => {
+  return (
     <>
-        <div className={classes.productTheme}>
-            <img src={nh_test} alt=""/>
-            <div>
-                <span>4월의 {props.theme}을 만나보세요</span>
-                <p><Link to={"/upload"}>4niture's {props.new} item</Link></p>
-            </div>
-        </div>    
+      <div className={classes.productTheme}>
+        <img src={nh_test} alt='' />
+        <div>
+          <span>4월의 {props.theme}을 만나보세요</span>
+          <p>
+            <Link to={"/product/upload"}>4niture's {props.new} item</Link>
+          </p>
+        </div>
+      </div>
     </>
-    )
+  );
 };
 
 export default ProductThemeFT;
