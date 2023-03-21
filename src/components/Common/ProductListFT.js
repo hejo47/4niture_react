14000980731f33a8a321d2ae89d569baad4fe5ba
naehyun25 <yoon658@naver.com>
2023-03-21 23:44:43 +0900

    230321 [윤내현] 메인new,best api연결

--- src/components/Common/ProductListFT.js
+++ src/components/Common/ProductListFT.js
@@ -1,80 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {  Link  } from "react-router-dom";
+import axios from "axios";
 import classes from "./ProducListFT.module.css";
-import nh_test from "../../img/nh_test.jpg";
-//import {API_URL} from "../config/Constans";;
+import { API_URL } from "../../config/constants";
 
-const NewProductList = [
-  {
-    name: "신상템이름1",
-    price: "10,000",
-    reviewNum: 15,
-  },
-  {
-    name: "신상템이름2",
-    price: "15,000",
-    reviewNum: 10,
-  },
-  {
-    name: "신상템이름3",
-    price: "10,000",
-    reviewNum: 10,
-  },
-  {
-    name: "신상템이름4",
-    price: "20,000",
-    reviewNum: 11,
-  },
-];
-const BestProductList = [
-  {
-    name: "베스트템이름1",
-    price: "10,000",
-    reviewNum: 15,
-  },
-  {
-    name: "베스트템이름2",
-    price: "15,000",
-    reviewNum: 10,
-  },
-  {
-    name: "베스트템이름3",
-    price: "10,000",
-    reviewNum: 10,
-  },
-  {
-    name: "베스트템이름4",
-    price: "20,000",
-    reviewNum: 11,
-  },
-];
 const ProductListFT = (props) => {
   let { dataname } = props;
-  const productList = dataname === "NewProductList" ? NewProductList : BestProductList;
+  const [products, setProduct]=useState([]);
+  useEffect(() => {
+    axios
+    .get(`${API_URL}/products/${dataname}`)
+    .then((result) => {
+      console.log(result)
+      const products=result.data.product
+      setProduct(products);
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  },[]);
+  
   return (
-    <>
       <div className={classes.productList}>
-        {productList.map((value) => {
-          let { name, price, reviewNum } = value;
+        {products.map((products,idx) => {
+          let { id, name, price, imageUrl } = products;
           return (
-            <>
-              <Link to='/product/new'>
-                <div className={classes.product}>
-                  <div className={classes.productImg}>
-                    <img src={nh_test} alt='' />
-                  </div>
-                  <div className={classes.productDescription}>
-                    <p className={classes.productName}>{name}</p>
-                    <p className={classes.productPrice}>{price}</p>
-                    <p className={classes.reviewNumber}>리뷰수{reviewNum}</p>
-                  </div>
+            <Link to={`/product/${id}`}>
+              <div className={classes.product}>
+                <div className={classes.productImg}>
+                  <img src={`${API_URL}/${imageUrl}`} alt={`${name}사진`} />
                 </div>
-              </Link>
-            </>
+                <div className={classes.productDescription}>
+                  <p className={classes.productName}>{name}</p>
+                  <p className={classes.productPrice}>{price}</p>
+                  <p className={classes.reviewNumber}>수정예정{price}</p>
+                </div>
+              </div>
+            </Link>
           );
         })}
       </div>
-    </>
   )};
 
 
