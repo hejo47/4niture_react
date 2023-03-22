f8f094aa8d5f99c4ddf91be4cdee02f1daf69b64
YoungBeomLee <edlee127127@gmail.com>
2023-03-22 16:06:29 +0900

    23-03-22 [이영범] reviewget 페이지 테스트

---
+++ src/components/pages/ReviewGetAD.js
@@ -0,0 +1,71 @@
+//견본파일입니다. 
+//상세페이지 링크 클릭해야합니다.
+
+import React, { useState, useEffect } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import classes from "./NewProductPageAD.module.css";
+import { Card, ConfigProvider, Col, Row } from "antd";
+import UploadAD from "./UploadAD";
+import axios from "axios";
+import { API_URL } from "./config/constants.js";
+
+
+const { Meta } = Card;
+const ReviewList = [
+  {
+    name: "신상템이름1",
+    price: "10,000",
+    reviewNum: 15,
+  },
+  {
+    name: "신상템이름2",
+    price: "15,000",
+    reviewNum: 10,
+  },
+  {
+    name: "신상템이름3",
+    price: "10,000",
+    reviewNum: 10,
+  },
+  {
+    name: "신상템이름4",
+    price: "20,000",
+    reviewNum: 11,
+  },
+];
+
+const ReviewGetAD = () => {
+  return (
+    <>
+      <h2>New Products</h2>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#F25A29",
+          },
+        }}
+      >
+        <Row gutter={12}>
+          {ReviewList.map((value) => {
+            let { name, price, reviewnum } = value;
+            return (
+              <>
+                <Routes>
+                  {/* <Route path='/product/upload' element={<UploadAD />}></Route> */}
+                </Routes>
+                <Col span={6}>
+                  <Card hoverable style={{ width: 240 }} cover={<img alt='example' src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png' />}>
+                    <Link to='/review/upload'>
+                      <Meta title={name} description={price} />
+                    </Link>
+                  </Card>
+                </Col>
+              </>
+            );
+          })}
+        </Row>
+      </ConfigProvider>
+    </>
+  );
+};
+export default ReviewGetAD;
