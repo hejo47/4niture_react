66ae3164a3b35ec3a88acb9146350c95677fae23
YoungBeomLee <edlee127127@gmail.com>
2023-03-23 22:21:48 +0900

    23-03-23 [이영범] 리뷰페이지 레이아웃 완성

--- src/components/pages/ReviewGetAD.js
+++ src/components/pages/ReviewGetAD.js
@@ -1,5 +1,5 @@
 /* eslint-disable array-callback-return */
-//견본파일입니다. 
+//견본파일입니다.
 //상세페이지 링크 클릭해야합니다.
 import React, { useState, useEffect } from "react";
 import { Link, Routes, Route } from "react-router-dom";
@@ -7,26 +7,23 @@ import { Card, ConfigProvider, Col, Row } from "antd";
 import axios from "axios";
 import { API_URL } from "../../config/constants";
 import ReviewSub from "./ReviewSubAD";
-
-
-
+import "./ReviewGetAD.css";
 const { Meta } = Card;
 
-
 const ReviewGetAD = () => {
-    const [reviews, setReviews] = useState([]);
-    useEffect(() => {
-        axios
-          .get(`${API_URL}/reviews`)
-          .then(function (result) {
-            const reviews = result.data.reviews;
-            setReviews(reviews);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-        }, []);
-    return (
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/reviews`)
+      .then(function (result) {
+        const reviews = result.data.reviews;
+        setReviews(reviews);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  return (
     <>
       <h2>Reviews</h2>
       <ConfigProvider
@@ -36,30 +33,28 @@ const ReviewGetAD = () => {
           },
         }}
       >
-        <Row gutter={12} >
-          
-              {reviews.map((review,idx) => {console.log(review)
-                let {name, productname, desc, imageUrl} = review;
-                
-                  return (
-                    <>
-                      <Routes>
-                        { <Route path='/review/upload' element={<ReviewSub />}></Route>  }
-                      </Routes>
-                      <Col span={6} key={idx}>
-                        <Card hoverable style={{ width: 240 }} cover={<img alt="review-img" src={`${API_URL}/${imageUrl}`} />}>
-                          <Link to='/review/upload'>
-                            <span>작성자:</span><Meta title={name}  />
-                            <span>상품명:</span><Meta title={productname}  />
-                            <span>리뷰:</span><Meta description={desc} />
-                          </Link>
-                        </Card>
-                      </Col>
-                    </>
-                  );
-                
-              })}
-          
+        <Row gutter={12}>
+          {reviews.map((review, idx) => {
+            console.log(review);
+
+            return (
+              <>
+                <Routes>{<Route path="/review/upload" element={<ReviewSub />}></Route>}</Routes>
+                <Col span={6} key={idx} className="reviewCard">
+                  <Card hoverable style={{ width: 240 }} cover={<img alt="review-img" src={`${API_URL}/${review.imageUrl}`} />}>
+                    <Link to="/review/upload">
+                      <span>작성자:</span>
+                      <Meta title={review.name} />
+                      <span>상품명:</span>
+                      <Meta title={review.productname} />
+                      <span>리뷰:</span>
+                      <Meta description={review.desc} />
+                    </Link>
+                  </Card>
+                </Col>
+              </>
+            );
+          })}
         </Row>
       </ConfigProvider>
     </>
