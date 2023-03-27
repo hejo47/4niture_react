7b4d71c170f925853312fdd7e58339e80f94e8da
YoungBeomLee <edlee127127@gmail.com>
2023-03-27 14:02:06 +0900

    23-03-27 [이영범] banner 이미지체인지

--- src/components/Common/BannerFT.js
+++ src/components/Common/BannerFT.js
@@ -9,47 +9,60 @@ import "swiper/css/effect-fade";
 import "swiper/css/autoplay";
 import styles from "./BannerFT.module.css";
 import "./BannerFT.css";
-import Banner1 from "../../img/banner/Slide-1.jpg";
-import Banner2 from "../../img/banner/Slide-2.jpg";
-import Banner3 from "../../img/banner/Slide-3.jpg";
-
+import Banner1 from "../../img/banner/SlideMDPick-1.jpg";
+import Banner2 from "../../img/banner/SlideMDPick-2.jpg";
+import Banner3 from "../../img/banner/SlideMDPick-3.jpg";
+import Banner4 from "../../img/banner/SlideMDPick-4.jpg";
+import Banner5 from "../../img/banner/SlideMDPick-5.jpg";
 
 const Swipe = () => {
   return (
     <>
-    <Swiper
-      className={styles}
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay]}
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      speed={1000}
-      effect={"fade"}
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log("slide change")}
-      autoplay={{
-        delay: 2000,
-        disableOnInteraction: false
-      }}
-      loop
-      
-    >
-      <SwiperSlide>
-        <div className={styles.Textbox} ><h2>Banner1 Textbox</h2></div>
-        <div class={styles.imgbox}><img src={Banner1} alt="의자1" /></div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className={styles.Textbox} ><h2>Banner2 Textbox</h2></div>
-        <div class={styles.imgbox}><img src={Banner2} alt="" /></div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className={styles.Textbox} ><h2>Banner3 Textbox</h2></div>
-        <div class={styles.imgbox}><img src={Banner3} alt="의자1" /></div>
-      </SwiperSlide>
-    </Swiper>
+      <Swiper
+        className={styles.Swiper}
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        speed={200}
+        effect={"fade"}
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        loop
+      >
+        <SwiperSlide>
+          <div class={styles.imgbox}>
+            <img src={Banner1} alt="의자1" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class={styles.imgbox}>
+            <img src={Banner2} alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class={styles.imgbox}>
+            <img src={Banner3} alt="의자1" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class={styles.imgbox}>
+            <img src={Banner4} alt="의자1" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class={styles.imgbox}>
+            <img src={Banner5} alt="의자1" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 };
