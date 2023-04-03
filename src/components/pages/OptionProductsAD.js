f4b6b3ee4aa8d5ad7bba2f711baa390974bf34bb
naehyun25 <yoon658@naver.com>
2023-04-03 12:14:02 +0900

    pull 전 commit

--- src/components/pages/OptionProductsAD.js
+++ src/components/pages/OptionProductsAD.js
@@ -42,12 +42,12 @@ const TotalProductPageAD = () => {
             }}
           >
             {items.map((item, idx) => {
-              let { name, price, imageUrl, desc } = item;
+              let { id, name, price, imageUrl, desc } = item;
               return (
                 <Col span={6} xs={24} md={12} sm={12} lg={6} className='reviewCard'>
                   <Card hoverable cover={<img alt={`${name}사진`} src={`${API_URL}/${imageUrl}`} />}>
                     {/* {console.log(imageUrl, name, price)} */}
-                    <Link to='/products/:id'>
+                    <Link to={`/products/${id}`}>
                       <Meta title={name} description={price} />
                       {/* <p>{desc}</p> */}
                     </Link>
